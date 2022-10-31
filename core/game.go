15baf2803367b9af1229549720ba4b62d021c0c7
Sean ONeill <seansaysmeh@gmail.com>
2022-10-31 11:52:49 +0000

    added game states

--- core/game.go
+++ core/game.go
@@ -8,17 +8,28 @@ import (
 	"time"
 )
 
+const menuGameState = "menu"
+const playingGameState = "playing"
+const gameOverGameState = "gameOver"
+
 type Game struct {
 	lastUpdateCalled time.Time
 	player           *Player
 	bullets          []*Bullet
 	timer            float64
+	state            string
+	images           map[string]*ebiten.Image
 }
 
 func NewGame() *Game {
 	return &Game{
-		player:  NewPlayer(),
-		bullets: []*Bullet{},
+		//player:  NewPlayer(),
+		//bullets: []*Bullet{},
+		state: menuGameState,
+		images: map[string]*ebiten.Image{
+			"game-over-text": common.LoadImage("game-over-text.png"),
+			"play-text":      common.LoadImage("play-text.png"),
+		},
 	}
 }
 
@@ -26,41 +37,70 @@ func (r *Game) Update() error {
 	delta := float64(time.Now().Sub(r.lastUpdateCalled).Milliseconds()) / 1000
 	r.lastUpdateCalled = time.Now()
 
+	switch r.state {
+	case menuGameState:
+		if ebiten.IsKeyPressed(ebiten.KeySpace) || ebiten.IsKeyPressed(ebiten.KeyEnter) {
+			r.StartNewGame()
+		}
+	case playingGameState:
+		r.player.Update(delta, r)
+		for _, b := range r.bullets {
+			b.Update(delta, r)
+		}
+
+		r.timer = r.timer + delta
+		if r.timer > 1.5 {
+			r.timer = 0
+			r.AddBullet(90, 10, 1)
+		}
+	case gameOverGameState:
+		if ebiten.IsKeyPressed(ebiten.KeySpace) || ebiten.IsKeyPressed(ebiten.KeyEnter) {
+			return common.NormalEscapeError
+		}
+	}
+
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
 		return common.NormalEscapeError
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
 		ebiten.SetFullscreen(!ebiten.IsFullscreen())
 	}
-	r.player.Update(delta, r)
-	for _, b := range r.bullets {
-		b.Update(delta, r)
-	}
-
-	r.timer = r.timer + delta
-	if r.timer > 1.5 {
-		r.timer = 0
-		r.AddBullet(90, 10, 1)
-	}
 
 	return nil
 }
 
 func (r *Game) Draw(screen *ebiten.Image) {
-	r.player.Draw(screen)
-	for _, b := range r.bullets {
-		b.Draw(screen)
+
+	switch r.state {
+	case menuGameState:
+		r.drawImage(screen, "play-text", 40, 40)
+	case playingGameState:
+		r.player.Draw(screen)
+		for _, b := range r.bullets {
+			b.Draw(screen)
+		}
+	case gameOverGameState:
+		r.player.Draw(screen)
+		for _, b := range r.bullets {
+			b.Draw(screen)
+		}
+		r.drawImage(screen, "game-over-text", 40, 40)
 	}
+
 }
 
 func (r *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return common.ScreenWidth * common.Scale, common.ScreenHeight * common.Scale
 }
 
+func (r *Game) StartNewGame() {
+	r.bullets = []*Bullet{}
+	r.player = NewPlayer()
+	r.state = playingGameState
+}
+
 func (r *Game) PlayerDeath() {
-	// change state..
-	// show game over text
-	// change to high scores screen
+	r.state = gameOverGameState
 }
 
 func (r *Game) AddBullet(x float64, y float64, dir int) {
@@ -78,4 +118,11 @@ func (r *Game) RemoveBullet(bullet *Bullet) {
 		}
 	}
 	r.bullets = newBullets
+}
+
+func (r *Game) drawImage(screen *ebiten.Image, img string, x float64, y float64) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(x, y)
+	op.GeoM.Scale(common.Scale, common.Scale)
+	screen.DrawImage(r.images[img], op)
 }
